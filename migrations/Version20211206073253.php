340f7ca6aae7a0bba2b775e98fb167237fc4f863
Rémi <remi.becquaert35@gmail.com>
2021-12-06 10:40:56 +0100

    add skills modifier form and type

--- migrations/Version20211206073253.php
+++ migrations/Version20211206073253.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211130143103 extends AbstractMigration
+final class Version20211206073253 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,7 +22,7 @@ final class Version20211130143103 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE competence (id INT AUTO_INCREMENT NOT NULL, parent_id INT DEFAULT NULL, main_comp_id INT DEFAULT NULL, libelle VARCHAR(30) NOT NULL, level INT NOT NULL, INDEX IDX_94D4687F727ACA70 (parent_id), INDEX IDX_94D4687FB9469222 (main_comp_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE posseder (id INT AUTO_INCREMENT NOT NULL, competence_id INT NOT NULL, user_id INT NOT NULL, nbr_recommander INT NOT NULL, INDEX IDX_62EF7CBA15761DAB (competence_id), INDEX IDX_62EF7CBAA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, date_inscription DATETIME NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, date_inscription DATETIME NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE competence ADD CONSTRAINT FK_94D4687F727ACA70 FOREIGN KEY (parent_id) REFERENCES competence (id)');
         $this->addSql('ALTER TABLE competence ADD CONSTRAINT FK_94D4687FB9469222 FOREIGN KEY (main_comp_id) REFERENCES competence (id)');
         $this->addSql('ALTER TABLE posseder ADD CONSTRAINT FK_62EF7CBA15761DAB FOREIGN KEY (competence_id) REFERENCES competence (id)');
